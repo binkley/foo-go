77d93610e7dcf07dd5fd80ad7d18c4211e6ae498
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-16 07:12:55 -0600

    Fund server
    It does not run single-threaded, so most time fails from concurrent
    modifications.

--- funding/fund_test.go
+++ funding/fund_test.go
@@ -1,17 +1,88 @@
 package funding
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
-func BenchmarkFund(b *testing.B) {
-	// Add as many dollars as we have iterations this run
-	fund := NewFund(b.N)
+type FundServer struct {
+	Commands chan interface{}
+	fund     *Fund
+}
 
-	// Burn through them one at a time until they are all gone
-	for i := 0; i < b.N; i++ {
-		fund.Withdraw(1)
+func NewFundServer(initialBalance int) *FundServer {
+	server := &FundServer{
+		Commands: make(chan interface{}),
+		fund:     NewFund(initialBalance),
 	}
 
-	if fund.Balance() != 0 {
-		b.Error("Balance wasn't zero:", fund.Balance())
+	go server.loop()
+
+	return server
+}
+
+func (s *FundServer) loop() {
+	for command := range s.Commands {
+		switch command.(type) {
+		case WithdrawCommand:
+			withdrawal := command.(WithdrawCommand)
+			s.fund.Withdraw(withdrawal.Amount)
+
+		case BalanceCommand:
+			getBalance := command.(BalanceCommand)
+			balance := s.fund.Balance()
+			getBalance.Response <- balance
+
+		default:
+			panic(fmt.Sprintf("Unknown command: %v", command))
+		}
+	}
+}
+
+type WithdrawCommand struct {
+	Amount int
+}
+
+type BalanceCommand struct {
+	Response chan int
+}
+
+const WORKERS = 10
+
+func BenchmarkFund(b *testing.B) {
+	// Skip N = 1
+	if b.N < WORKERS {
+		return
+	}
+
+	// Add as many dollars as we have iterations this run
+	server := NewFundServer(b.N)
+
+	dollarsPerFounder := b.N / WORKERS
+
+	var wg sync.WaitGroup
+
+	// Burn through them one at a time until they are all gone
+	for i := 0; i < WORKERS; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < dollarsPerFounder; i++ {
+				server.Commands <- WithdrawCommand{Amount: 1}
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	balanceResponseChan := make(chan int)
+	server.Commands <- BalanceCommand{Response: balanceResponseChan}
+	balance := <-balanceResponseChan
+
+	if balance != 0 {
+		b.Error("Balance wasn't zero:", balance)
 	}
 }
