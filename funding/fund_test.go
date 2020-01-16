473186645455054f17ef0b910117369b0af793ef
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-16 05:46:13 -0600

    Formatting

--- funding/fund_test.go
+++ funding/fund_test.go
@@ -3,15 +3,15 @@ package funding
 import "testing"
 
 func BenchmarkFund(b *testing.B) {
-    // Add as many dollars as we have iterations this run
-    fund := NewFund(b.N)
+	// Add as many dollars as we have iterations this run
+	fund := NewFund(b.N)
 
-    // Burn through them one at a time until they are all gone
-    for i := 0; i < b.N; i++ {
-        fund.Withdraw(1)
-    }
+	// Burn through them one at a time until they are all gone
+	for i := 0; i < b.N; i++ {
+		fund.Withdraw(1)
+	}
 
-    if fund.Balance() != 0 {
-        b.Error("Balance wasn't zero:", fund.Balance())
-    }
+	if fund.Balance() != 0 {
+		b.Error("Balance wasn't zero:", fund.Balance())
+	}
 }
