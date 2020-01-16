1e34548ace7569ca43d96be638dd792dcce9ff52
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-16 07:08:31 -0600

    This go is not single-threaded

--- foo-go.go
+++ foo-go.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"github.com/binkley/foo-go/funding"
 	"rsc.io/quote"
+	"runtime"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
 
+	fmt.Println()
+
 	fund := funding.NewFund(3)
 	fmt.Println("You started with", fund.Balance(), "zorkmids.")
 	fund.Withdraw(1)
 	fmt.Println("You now have", fund.Balance(), "zorkmids.")
+
+	fmt.Println()
+
+	fmt.Println("GOMAXPROCS is", runtime.GOMAXPROCS(0))
 }
