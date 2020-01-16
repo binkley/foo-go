25d4a83030ae81c81528a6d7a86c01abbf76566a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-16 05:19:47 -0600

    Funding

--- foo-go.go
+++ foo-go.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/binkley/foo-go/funding"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
+
+	fund := funding.NewFund(3)
+	fmt.Println("You started with", fund.Balance(), "zorkmids.")
+	fund.Withdraw(1)
+	fmt.Println("You now have", fund.Balance(), "zorkmids.")
 }
