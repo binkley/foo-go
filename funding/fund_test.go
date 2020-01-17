a5256f53683fbb6d1e51e90b9736b9f6061e6c3c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-17 06:37:41 -0600

    Code organization

--- funding/fund_test.go
+++ funding/fund_test.go
@@ -5,61 +5,6 @@ import (
 	"testing"
 )
 
-type Transactor func(fund *Fund)
-
-type TransactionCommand struct {
-	Transactor Transactor
-	Done       chan bool
-}
-
-type FundServer struct {
-	commands chan TransactionCommand
-	fund     *Fund
-}
-
-func NewFundServer(initialBalance int) *FundServer {
-	server := &FundServer{
-		commands: make(chan TransactionCommand),
-		fund:     NewFund(initialBalance),
-	}
-
-	go server.loop()
-
-	return server
-}
-
-func (s *FundServer) Transact(transactor Transactor) {
-	command := TransactionCommand{
-		Transactor: transactor,
-		Done:       make(chan bool),
-	}
-	s.commands <- command
-	<-command.Done
-}
-
-func (s *FundServer) Balance() int {
-	var balance int
-
-	s.Transact(func(f *Fund) {
-		balance = f.Balance()
-	})
-
-	return balance
-}
-
-func (s *FundServer) Withdraw(amount int) {
-	s.Transact(func(f *Fund) {
-		f.Withdraw(amount)
-	})
-}
-
-func (s *FundServer) loop() {
-	for transaction := range s.commands {
-		transaction.Transactor(s.fund)
-		transaction.Done <- true
-	}
-}
-
 const WORKERS = 10
 
 func BenchmarkFund(b *testing.B) {
