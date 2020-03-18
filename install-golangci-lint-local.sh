247410b1210d0007bb1b2c39c330e362ea4c89d9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-03-18 09:42:48 -0500

    Bump lint

--- install-golangci-lint-local.sh
+++ install-golangci-lint-local.sh
@@ -1,2 +1,2 @@
 # For Linux ; on MacOS, use homebrew
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.23.6
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.24.0
