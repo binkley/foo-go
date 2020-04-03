efb86e6767cdadc7974bac719fd9269ad22144b2
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-04-03 06:31:44 -0500

    Smarter download of golangci-lint

--- install-golangci-lint-local.sh
+++ install-golangci-lint-local.sh
@@ -1,2 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
 # For Linux ; on MacOS, use homebrew
+# golangci-lint has version 1.24.0 built from 6fd4383 on 2020-03-15T11:38:02Z
+
+if [[ -x bin/golangci-lint ]] ; then
+    case "$(bin/golangci-lint --version)" in
+    *1.24.0* ) exit 0 ;;
+    esac
+fi
+
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.24.0
